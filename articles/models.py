89899b3a3c7c8a7930de1b92d11c15d5578e6cdc
yungyikim <yungyikim@gmail.com>
2019-11-09 03:37:29 +0900

    게시판 모델 추가

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+class Article(models.Model):
+    id = models.AutoField(
+        primary_key=True
+    )
+    subject = models.CharField(
+        max_length=128,
+        null=False,
+        help_text='게시물 제목'
+    )
+    content = models.TextField(
+        null=True,
+        help_text='HTML형태의 게시물 내용'
+    )
+    create_date = models.DateTimeField(
+        auto_now_add=False,
+        db_index=True
+    )
+    modified_date = models.DateTimeField(
+        auto_now_add=False
+    )
+
+    class Meta:
+        ordering = ('id',)
+
+    def __str__(self):
+        return '%s' % self.id
