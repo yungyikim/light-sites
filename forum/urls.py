4f8253b5c0b4b83c943995741f3f08d22e2f6303
yungyikim <yungyikim@gmail.com>
2019-11-13 15:46:06 +0900

    홈 path 추가

--- forum/urls.py
+++ forum/urls.py
@@ -5,5 +5,5 @@ from forum import views
 app_name = 'forum'
 urlpatterns = [
     re_path(r'(?P<pk>[0-9]+)$', views.detail, name='detail'),
-    re_path('/?', views.list, name='list'),
+    re_path(r'^/?$', views.list, name='list'),
 ]
